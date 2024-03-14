d3bd46f9f3305c71a0c0b089a5ee79745b96c5d1
Vovaadidas777 <mateberidze464@gmail.com>
2024-03-14 23:07:32 +0400

    uptade commit

--- day46/script.js
+++ day46/script.js
@@ -3,22 +3,20 @@ console.log(5 + 3); // პასუხი: 8
 console.log(-7 + 10); // პასუხი: 3
 console.log(2.5 + 1.5); // პასუხი: 4
 
-// Subtraction
 console.log(10 - 7); // პასუხი: 3
 console.log(4.5 - 2.3); // პასუხი: 2.2
 console.log(-8 - 3); // პასუხი: -11
 
-// Multiplication
+
 console.log(5 * 4); // პასუხი: 20
 console.log(-2 * 6); // პასუხი: -12
 console.log(2.5 * 3); // პასუხი: 7.5
 
-// Division
+
 console.log(10 / 2); // პასუხი: 5
 console.log(15 / 4); // პასუხი: 3.75
 console.log(8 / 3); // პასუხი: 2.6666666666666665
 
-// Power
 console.log(2 ** 3); // პასუხი: 8
 console.log(5 ** 2); // პასუხი: 25
 console.log(10 ** 0.5); // პასუხი: 3.1622776601683795
