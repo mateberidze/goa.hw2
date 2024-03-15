279c92ce8add00ada393f871f373e769f934cbf6
Vovaadidas777 <mateberidze464@gmail.com>
2024-03-15 21:12:27 +0400

    uptade commit

---
+++ day46/script.js
@@ -0,0 +1,38 @@
+//მიმატება
+console.log(5 + 3); // პასუხები: 8
+console.log(-7 + 10); // პასუხები: 3
+console.log(2.5 + 1.5); // პასუხები: 4
+
+//გამოკლება
+console.log(10 - 7); // პასუხები: 3
+console.log(4.5 - 2.3); // პასუხები: 2.2
+console.log(-8 - 3); // პასუხები: -11
+
+//გამრავლება
+console.log(5 * 4); // პასუხები: 20
+console.log(-2 * 6); // პასუხები: -12
+console.log(2.5 * 3); // პასუხები: 7.5
+
+//გაყოფა
+console.log(10 / 2); // პასუხები: 5
+console.log(15 / 4); // პასუხები: 3.75
+console.log(8 / 3); // პასუხები: 2.6666666666666665
+
+//ხარისხი
+console.log(2 ** 3); // პასუხები: 8
+console.log(5 ** 2); // პასუხები: 25
+console.log(10 ** 0.5); // პასუხები: 3.1622776601683795
+
+//შედარების ოპერატორი
+console.log(5 > 3); // პასუხი: true
+console.log(10 <= 7); // პასუხი: false
+console.log(4 === 4); // პასუხი: true
+console.log(2 !== '2'); // პასუხი: true
+console.log(8 >= 8); // პასუხი: true
+
+// ლოგიკური ოპერატორი
+console.log(true && false); // პასუხი: false
+console.log(true || false); // პასუხი: true
+console.log(!true); // პასუხი: false
+console.log(!(5 < 3)); // პასუხი: true
+console.log((5 > 3) && (10 <= 7)); // პასუხი: false
