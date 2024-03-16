ed3841d9af75a756e5863edbf7d46ae8624fded1
Vovaadidas777 <mateberidze464@gmail.com>
2024-03-16 21:14:47 +0400

    commit

---
+++ day47/script.js
@@ -0,0 +1,21 @@
+
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const photo = document.getElementById('photo');
+
+
+btn1.addEventListener('click', function() {
+  photo.style.width = '300px';
+  alert('Photo width set to 300');
+});
+
+btn2.addEventListener('click', function() {
+  photo.style.width = '500px';
+  alert('Photo width set to 500');
+
+
+
+});const num1 = 10;
+const num2 = 20;
+
+console.log("Sum of num1 and num2:", num1 + num2);
