0eabf770bde23beefedeca07281496c333d88425
Vovaadidas777 <mateberidze464@gmail.com>
2024-03-16 21:27:49 +0400

    uptade commit

--- day47/script.js
+++ day47/script.js
@@ -11,11 +11,11 @@ btn1.addEventListener('click', function() {
 
 btn2.addEventListener('click', function() {
   photo.style.width = '500px';
-  alert('Photo width set to 500');
-
-
-//davaleba2
-});const num1 = 10;
+  alert('Photo width set to 500')});
+  
+  
+  //davaleba2
+const num1 = 10;
 const num2 = 20;
 
 console.log("Sum of num1 and num2:", num1 + num2);
